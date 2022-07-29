65a82ac189f9b73f570a0e5bd77a6805fab9a969
kuangshp <kuangshp@126.com>
2022-07-29 08:31:12 +0800

    feat: 新增

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App';
+import router from './router/index';
+import store from './store';
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Print from 'vue-print-nb';
+import echarts from 'echarts';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI, {
+  size: 'small',
+  zIndex: 3000,
+});
+Vue.use(Print);
+Vue.prototype.$echarts = echarts;
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
