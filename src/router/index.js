6e725c5a1f6ce51a935d06522e16687d33ee7c74
kuangshp <kuangshp@126.com>
2022-07-29 09:52:10 +0800

    feat: 新增

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,82 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home,
+    component: () => import('@/views/Home/Home'),
+    redirect: '/home/main',
+    children: [
+      {
+        path: 'main',
+        name: 'Main',
+        component: () => import('@/views/Main/Main'),
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'waiting',
+        name: 'Waiting',
+        component: () => import('@/views/Waiting/Waiting'),
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'admin',
+        name: 'Admin',
+        component: () => import('@/views/Admin/Admin'),
+        meta: {
+          requiresAuth: true,
+        },
+        children: [
+          {
+            path: 'user',
+            name: 'User',
+            component: () => import('@/components/admin/UserAdmin/UserAdmin'),
+            meta: {
+              requiresAuth: true,
+            },
+          },
+          {
+            path: 'schedule',
+            name: 'ScheduleAdmin',
+            component: () => import('@/components/admin/ScheduleAdmin/ScheduleAdmin'),
+            meta: {
+              requiresAuth: true,
+            },
+          },
+          {
+            path: 'registrationlevel',
+            name: 'RegistrationLevelAdmin',
+            component: () => import('@/components/admin/RegistrationLevelAdmin/RegistrationLevelAdmin'),
+            meta: {
+              requiresAuth: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login/Login.vue'),
+    component: () => import('@/views/Login/Login'),
+    meta: {
+      requiresAuth: false,
+    },
+  },
+  {
+    path: '/caller',
+    name: 'Caller',
+    component: () => import('@/views/Caller/Caller'),
+    meta: {
+      requiresAuth: false,
+    },
   },
 ];
 
